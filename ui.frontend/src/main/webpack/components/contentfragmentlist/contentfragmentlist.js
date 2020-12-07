f545f5b77a4e29b53abe5f1d8fd6562892e7dc30
Mark Szulc <mszulc@adobe.com>
2020-12-08 10:29:01 +1100

    Additional Stylining & Footer

--- ui.frontend/src/main/webpack/components/contentfragmentlist/contentfragmentlist.js
+++ ui.frontend/src/main/webpack/components/contentfragmentlist/contentfragmentlist.js
@@ -1,18 +1,17 @@
-/* JS Snippet to trigger image replacement for Content Fragment List */
-var jQuery = require("jquery");
 
-jQuery(function (element,$) {
-    "use strict";
-
-    $('.cmp-contentfragment__element--creditcardimage').each(function(){
-       
-        var img = document.createElement("img");
-        img.src = $(this).find("dd").text();
-        var $imgDiv = $('<div class="cmp-contentfragment_image"></div>').append(img);
-		$(this).parent().append($imgDiv);
+var images = document.getElementsByClassName('cmp-contentfragment__element--creditCardImage');
 
 
+Array.prototype.forEach.call(images, function(image){
 
-     });
+var img = document.createElement("img");
+img.src = image.querySelector('dd').textContent;
 
-}('body',jQuery));
+// Create a new element
+var newNode = document.createElement('div');
+newNode.className = 'cmp-contentfragment_image';
+newNode.append(img);
+
+image.prepend(newNode);
+});
+  
