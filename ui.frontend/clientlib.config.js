120828d82d0a4b286418c3c7180508bbcc8a1550
Mark Szulc <mszulc@adobe.com>
2021-03-06 20:53:22 +1100

    rebase

--- ui.frontend/clientlib.config.js
+++ ui.frontend/clientlib.config.js
@@ -26,7 +26,7 @@ const CLIENTLIB_DIR = path.join(
   'content',
   'jcr_root',
   'apps',
-  'apacdemo',
+  'mysite',
   'clientlibs'
 );
 
@@ -45,7 +45,7 @@ module.exports = {
     {
       ...libsBaseConfig,
       name: 'clientlib-dependencies',
-      categories: ['apacdemo.dependencies'],
+      categories: ['mysite.dependencies'],
       assets: {
         // Copy entrypoint scripts and stylesheets into the respective ClientLib
         // directories
@@ -64,8 +64,8 @@ module.exports = {
     {
       ...libsBaseConfig,
       name: 'clientlib-site',
-      categories: ['apacdemo.site'],
-      dependencies: ['apacdemo.dependencies'],
+      categories: ['mysite.site'],
+      dependencies: ['mysite.dependencies'],
       assets: {
         // Copy entrypoint scripts and stylesheets into the respective ClientLib
         // directories
